950e826e1573dbde1624e899b6fd5b26bb10e7a4
Minh Le Hoang <hoale@microsoft.com>
2018-12-30 14:38:22 -0800

    Add css into project

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import "./index.scss";
+
 alert("test");
 let a: number = 10;
 a = 1;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // to build from html template
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // to extract css into it own file
 
 let plugins = [
     new HtmlWebpackPlugin({
         template: './src/index.html'
     }),
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].[hash].css',
+        chunkFilename: '[id].[hash].css'
+    })
 ];
 
 module.exports = {
@@ -20,11 +27,31 @@ module.exports = {
     devtool: "source-map",
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts", ".tsx", ".js", ".json", ".css", ".scss"]
     },
 
     module: {
         rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    // Replace 'style-loader' but still need to install it
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { test: /\.tsx?$/, loader: "ts-loader" },
 
